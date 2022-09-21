39bd50e455a69d72f50d98e00b14bc6218917170
Walessonrdreis <wallllase@hotmail.com>
2022-09-20 21:48:27 -0300

    add:teste unitário: Classe calculadora, soma

--- testesUnitarios/src/main/java/model/Calculadora.java
+++ testesUnitarios/src/main/java/model/Calculadora.java
@@ -1,12 +1,15 @@
 package model;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
+    Scanner teclado = new Scanner(System.in);
 
-    public int soma(int primeiroNumero, int segundNumero) {
-         int n1 = 1;
-         int n2 = 1;
-         int soma = n1 + n1;
+    public int soma(int primeiroNumero, int segundoNumero) {
+         int n1 =1;
+         int n2 =1;
+         int soma = n1 + n2;
         return soma;
     }
 }
