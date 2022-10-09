ed2453b366dd407c415de1cb7b6ed3e143841148
Walessonrdreis <wallllase@hotmail.com>
2022-10-09 20:53:02 -0300

    create: modulo estruturasCondicionais: classe ResultadoEscolar ‘update’, estrutura composta

--- estruturasCondicionais/src/main/java/org/example/model/ResultadoEscolar.java
+++ estruturasCondicionais/src/main/java/org/example/model/ResultadoEscolar.java
@@ -2,10 +2,12 @@ package org.example.model;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 9;
-        if (nota >= 7){
+        int nota = 6;
+        if (nota >= 7) {
             System.out.println("Aluno Aprovado");
-        }else{
+        }else if (nota >= 5 &&  nota < 7){
+            System.out.println("Recuperação");
+            } else{
             System.out.println("Aluno Reprovado");
         }
     }
