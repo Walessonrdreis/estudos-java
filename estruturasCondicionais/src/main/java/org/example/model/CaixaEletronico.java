880f06e38289ccbc3c35cf919a35cbe56ef4eb92
Walessonrdreis <wallllase@hotmail.com>
2022-10-09 20:32:07 -0300

    create: modulo estruturasCondicionais: classe CaixaEletronico, condicional simples

---
+++ estruturasCondicionais/src/main/java/org/example/model/CaixaEletronico.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 27.0;
+
+        if(valorSolicitado < saldo){
+            saldo -= valorSolicitado;
+
+            System.out.println(saldo);
+        }
+    }
+}
