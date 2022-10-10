80278c03819c87636f91c91e3ef03c2a3036c948
Walessonrdreis <wallllase@hotmail.com>
2022-10-09 21:00:35 -0300

    create: modulo estruturasCondicionais: classe ResultadoEscolar ‘update’ if ternario exemplo, estrutura composta

--- estruturasCondicionais/src/main/java/org/example/model/ResultadoEscolar.java
+++ estruturasCondicionais/src/main/java/org/example/model/ResultadoEscolar.java
@@ -10,5 +10,14 @@ public class ResultadoEscolar {
             } else{
             System.out.println("Aluno Reprovado");
         }
+        //if ternário
+        int nota1 = 7;
+        String resultado1 = nota1 >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado1);
+
+        int nota2 = 6;
+        //if ternário com else if
+        String resultado2 = nota2 >=7 ? "Aprovado" : nota2 >= 5 && nota2 < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado2);
     }
 }
