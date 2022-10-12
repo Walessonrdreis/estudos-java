245b0506219ae51a4ab8c5b06ee3e1ec7827fb02
Walessonrdreis <wallllase@hotmail.com>
2022-10-12 09:30:38 -0300

    create module: estruturasderepeticao, exemple for 01

---
+++ estruturasDeRepeticao/src/main/java/org/example/model/ExempleFor.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+public class ExempleFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println(carneirinhos + " -Carneirinhos(s)");
+        }
+
+        //Modelo 02
+        int carneiros02 = 1;
+        for (;carneiros02 <=20;){
+            System.out.println(carneiros02 + " - Carneirinho(s)");
+            carneiros02 ++;
+        }
+    }
+
+}
