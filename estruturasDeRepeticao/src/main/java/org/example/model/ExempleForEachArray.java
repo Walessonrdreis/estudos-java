0217f83c805d6c35abfd33f29209ca5d22cb525a
Walessonrdreis <wallllase@hotmail.com>
2022-10-12 09:53:32 -0300

    module: estruturasderepeticao, exemple for 02 usando array com for/each

---
+++ estruturasDeRepeticao/src/main/java/org/example/model/ExempleForEachArray.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class ExempleForEachArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Walas", "Daniela", "Almeir", "José Carlos" };
+        for(String aluno : alunos){
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+    }
+
+
+}
